0910b4941a1a9ce44ba145b841a29fa0cbd98aa2
ookawa0602 <forefront.ookawa2@gmail.com>
2021-03-21 23:30:38 +0900

    お知らせ追加

--- resources/views/components/titosedai.blade.php
+++ resources/views/components/titosedai.blade.php
@@ -11,9 +11,17 @@
   <img class="img1"src="{{ asset('img/wave2.JPG') }}"alt="千歳台 クリーニング 世田谷区千歳台 シミ抜き とことん綺麗
 に クリーニングWAVE 店内"/>
 </p>
-<h1 class="news">お知らせ</h1>
+
+ <h6 class="news">お知らせ</h6>
+  <div class="flame17">
+   <h2 class="kokuti">春のクリーニングセール!!!</h2>
+   <h2 class="hiduke">3月15日〜5月30日(日)</h2>
+   <h2 class="hukudai">弦巻店、千歳船橋店両店対象</h2>
+    <h2 class="kokuti1">20%OFF</h2>
+  </div>
 <script src="{{ asset('js/img.js') }}"></script>
 </body>
+
 
 
 
--- resources/views/components/turumaki.blade.php
+++ resources/views/components/turumaki.blade.php
@@ -9,6 +9,13 @@
   <img class="img1"src="{{ asset('img/turu2.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE 店内1">
   <img class="img1"src="{{ asset('img/wave7.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE 店内外観">
 </p>
-<h6 class="news">お知らせ</h6>
+
+ <h6 class="news">お知らせ</h6>
+  <div class="flame17">
+   <h2 class="kokuti">春のクリーニングセール!!!</h2>
+   <h2 class="hiduke">3月15日〜5月30日(日)</h2>
+   <h2 class="hukudai">弦巻店、千歳船橋店両店対象</h2>
+    <h2 class="kokuti1">20%OFF</h2>
+  </div>
 <script src="{{ asset('js/img.js') }}"></script>
 </body>
