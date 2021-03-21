be1dce4bda755b2103d36b655f21620054a983ac
ookawa0602 <forefront.ookawa2@gmail.com>
2021-03-21 16:22:31 +0900

    system add1

--- resources/views/components/titosedai.blade.php
+++ resources/views/components/titosedai.blade.php
@@ -3,11 +3,11 @@
  <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
 
 <p id="slideshow">
-  <img class="img1"src="{{ asset('img/logoname.JPG') }}" alt="wave2">
-  <img class="img1"src="{{ asset('img/titose.JPG') }}" alt="wave3">
-  <img class="img1"src="{{ asset('img/wave2.JPG') }}" alt="logos">
-  <img class="img1"src="{{ asset('img/wave3.jpg') }}" alt="logos">
-  <img class="img1"src="{{ asset('img/wave6.JPG') }}">
+   <img class="img1"src="{{ asset('img/logoname.JPG') }}" alt="千歳台 クリーニング 世田谷区千歳台 シミ抜き とことん綺麗に クリーニングWAVE ロゴ"/>
+  <img class="img1"src="{{ asset('img/wave1.JPG') }}" alt="千歳台 クリーニング 世田谷区千歳台 シミ抜き とことん綺麗
+に クリーニングWAVE 店舗外観"/>
+  <img class="img1"src="{{ asset('img/wave2.JPG') }}"alt="千歳台 クリーニング 世田谷区千歳台 シミ抜き とことん綺麗
+に クリーニングWAVE 店内"/>
 </p>
 <script src="{{ asset('js/img.js') }}"></script>
 </body>
--- resources/views/components/turumaki.blade.php
+++ resources/views/components/turumaki.blade.php
@@ -1,11 +1,14 @@
 <body>
+ <link rel="stylesheet" href="{{ asset('css/body.css') }}">
  <link rel="stylesheet" href="{{ asset('css/img.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/design.css') }}">
  <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
 
 <p id="slideshow"> 
   <img class="img1"src="{{ asset('img/logoname.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE ロゴ">
   <img class="img1"src="{{ asset('img/turu2.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE 店内1">
-  <img class="img1"src="{{ asset('img/turu3.JPG') }}" alt="logos">
+  <img class="img1"src="{{ asset('img/wave7.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE 店内外観">
 </p>
+<h6 class="news">お知らせ</h6>
 <script src="{{ asset('js/img.js') }}"></script>
 </body>
