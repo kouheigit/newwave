48f20d17821b02543e346ad724555bc7ad1f9f03
ookawa0602 <forefront.ookawa2@gmail.com>
2021-03-21 12:31:10 +0900

    system add

--- resources/views/components/titosedai.blade.php
+++ resources/views/components/titosedai.blade.php
@@ -3,7 +3,7 @@
  <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
 
 <p id="slideshow">
-  <img class="img1"src="{{ asset('img/logos.JPG') }}" alt="wave2">
+  <img class="img1"src="{{ asset('img/logoname.JPG') }}" alt="wave2">
   <img class="img1"src="{{ asset('img/titose.JPG') }}" alt="wave3">
   <img class="img1"src="{{ asset('img/wave2.JPG') }}" alt="logos">
   <img class="img1"src="{{ asset('img/wave3.jpg') }}" alt="logos">
--- resources/views/components/turumaki.blade.php
+++ resources/views/components/turumaki.blade.php
@@ -3,11 +3,9 @@
  <script src="https://code.jquery.com/jquery-1.12.4.min.js" type="text/javascript"></script>
 
 <p id="slideshow"> 
- <img class="img1"src="{{ asset('img/logos.JPG') }}" alt="wave2">
-  <img class="img1"src="{{ asset('img/turu2.JPG') }}" alt="wave3">
+  <img class="img1"src="{{ asset('img/logoname.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE ロゴ">
+  <img class="img1"src="{{ asset('img/turu2.JPG') }}" alt="弦巻 クリーニング 世田谷区弦巻 シミ抜き とことん綺麗に クリーニングWAVE 店内1">
   <img class="img1"src="{{ asset('img/turu3.JPG') }}" alt="logos">
-  <img class="img1"src="{{ asset('img/wave3.jpg') }}" alt="logos">
-  <img class="img1"src="{{ asset('img/turu4.JPG') }}">
 </p>
 <script src="{{ asset('js/img.js') }}"></script>
 </body>
